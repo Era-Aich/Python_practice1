2cfa24253f3626ef74269b45f4513a4d82889e53
Era Aich <45397193+Era-Aich@users.noreply.github.com>
2023-01-16 18:55:35 +0600

    Add files via upload

---
+++ reverse_array.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 16 17:16:47 2023
+
+@author: asus
+"""
+from array import *
+arr=array('i',[1,2,3,4,5,6,7,8])
+arr.remove(5)
+print(arr)
+arr.insert(2,10)
+print(arr)
+arr.pop(2)
+print(arr)
+    
