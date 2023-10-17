e54ea77957f369f0264d3c49172ff9ee7f5163d4
Era Aich <45397193+Era-Aich@users.noreply.github.com>
2023-10-17 16:29:14 +0530

    Add files via upload

---
+++ Laplacian_matrix.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 15 19:28:33 2023
+
+@author: asus
+"""
+
+from sklearn.neighbors import NearestNeighbors
+import numpy.float as np
+import pandas as pd
+#from sklearn.neighbors import NearestNeighbors
+
+data= pd.read_csv("D:\\SAU_2nd_Semester\\ASSIGNMENT_Python\\survey lung cancer.csv")
